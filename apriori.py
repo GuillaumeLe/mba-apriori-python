d223fe6714fcaf12ea19c32f5a433f82de5f092a
Guillaume LEROY <gleroy@ensta.fr>
2016-12-09 11:51:43 +0100

    Apriori implement but doesn't pass test

--- apriori.py
+++ apriori.py
@@ -1,11 +1,151 @@
+from itertools import chain, combinations
+import itertools
+
 def getDistinctElt(transactions):
+    """
+        Get the distinct element in a list of transactions.
+    """
     distinctSet = set()
     for transaction in transactions:
         for elt in transaction:
             if not(elt in distinctSet):
                 distinctSet.add(elt)
-    return distinctSet 
+    return distinctSet
 
-def getOccurence(elt, transactions):
+def getOccurence(item, transactions):
+    """
+        Get the number of occurence in a list of trnasactions
+    """
     occurence = 0
-    return 3
+    for transaction in transactions:
+        for elt in transaction:
+            if elt == item:
+                occurence += 1
+    return occurence
+
+def strToSet(string):
+    """
+        Convert a string into a set
+    """
+    ens = set()
+    for char in string:
+        ens.add(char)
+    return ens
+
+def setToStr(ens):
+    """
+        Convert a set into a string.
+    """
+    string = ""
+    ens = sorted(ens)
+    for elt in ens:
+        string += str(elt)
+    return string
+
+def getSubset(iterable):
+    """ Get all subset of iterable """
+    xs = list(iterable)
+    # note we return an iterator rather than a list
+    subsets_tuple = list(chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1)))
+    subsets = [set(sub) for sub in subsets_tuple]
+    return subsets
+
+def setRules(occurence, conf):
+    """
+    Formate the rules from the occurence table and conserve only rules with
+    enought confidence.
+    INPUT
+        occurence (list) : the table of occurence
+        conf : the target confidence
+    OUTPUT
+        a list of rules where a rules is defined (pre, post, conf) which
+        means : pre => post with conf confidence.
+    """
+
+    rules = []
+    for level in occurence[:0:-1]:
+        for item in level:
+            ens = strToSet(item[0])
+            occ = item[1]
+            for elt in getSubset(ens):
+                if (set(elt) != set()) and (set(elt) != ens):
+                    for ref in occurence[len(elt)-1]:
+                        if set(elt) == strToSet(ref[0]):
+                            occ_ref = ref[1]
+                    confidence = occ/occ_ref
+                    if confidence >= conf:
+                        rules.append((set(elt), set(ens.difference(elt)), confidence))
+    return rules
+def getLabels(occurence_dict):
+    """
+    Get the associations in occurence_dict without occurence
+    INPUT
+        occurence_dict (list) : contains the associations and occurence
+    OUTPUT
+        a list of the associations in occurence_dict
+    """
+    labels = []
+    for elt in occurence_dict:
+        labels.append(elt[0])
+    return labels
+
+def apriori(transactions, min_supp=3, confidence=0.75):
+    """
+    Run the apriori algorithme to found rules.
+    INPUT
+        transactions (list) : is a list of transactions wich is list of items.
+        min_supp (int) : is the minimal support to consider a rules.
+        confidence (float) : is the minimal confidence to consider a rules.
+    OUTPUT
+        A list of rules where each item contains (pre, post, confidence).
+    """
+    # get the distinct elt in transactions
+    disctinct_elt = getDistinctElt(transactions)
+
+    # a list that contains the rules, occurence_dict[n-1] contains the rules of
+    # length n
+    occurence_dict = []
+
+    # labels contains the the association of item that we have conserved.
+    # labels[n-1] contains set of association with n items
+    labels = []
+
+    # get the rules of length 1
+    occurence_dict_current = []
+    for item in disctinct_elt:
+        # get the occurence of the item in transactions
+        occurence = getOccurence(item, transactions)
+        if occurence >= min_supp:
+            occurence_dict_current.append((item,occurence))
+    occurence_dict.append(occurence_dict_current)
+    print(occurence_dict)
+    # We add the associations converded
+    labels.append(set(getLabels(occurence_dict[-1])))
+    print(labels)
+    n = 2
+    # if the last item of occurence_dict is empty we've finished the algorithme
+    while occurence_dict[-1] != []:
+
+        # Building the cartesian product of associations of n-1 element and 1
+        # element.
+        cart = []
+        for i in itertools.product(labels[0], labels[n-2]):
+            if strToSet(i[0]).intersection(strToSet(i[1])) == set() and not(strToSet(i[0]).union(strToSet(i[1])) in cart):
+                cart.append(strToSet(i[0]).union(strToSet(i[1])))
+        print(cart)
+        # finding the occurence of association of n elements
+        occurence_dict_current = []
+        for item in cart:
+            occurence = getOccurence(list(item), transactions)
+            if occurence >= min_supp:
+                occurence_dict_current.append((set_to_str(item),occurence))
+        occurence_dict.append(occurence_dict_current)
+        labels.append(set(getLabels(occurence_dict[-1])))
+        n += 1
+
+    # Once we have all the associations and their occurences we formate the
+    # rules
+    print(occurence_dict)
+    rules = setRules(occurence_dict[:-1], confidence)
+
+    return rules
--- test.py
+++ test.py
@@ -1,29 +1,100 @@
 import unittest
 from apriori import *
+
 class AprioriTest(unittest.TestCase):
     def test_getDistinctElt(self):
         transactions = [
-            ['A','B','D','E'], 
-            ['C','A','D','B','F'], 
-            ['D','A','H','B','C'], 
+            ['A','B','D','E'],
+            ['C','A','D','B','F'],
+            ['D','A','H','B','C'],
             ['E','F','B','C','E']
         ]
-        self.assertEqual(getDistinctElt(transactions), set(['A','B','C','D','E','F','H']))
+        self.assertEqual(
+            getDistinctElt(transactions),
+            set(['A','B','C','D','E','F','H'])
+        )
         transactions = [
             ['A','B','C'],
             ['A','C','D']
         ]
-        self.assertEqual(getDistinctElt(transactions), set(['A','B','C','D']))
-        
+        self.assertEqual(
+            getDistinctElt(transactions),
+            set(['A','B','C','D'])
+        )
+
     def test_getOccurence(self):
         transactions = [
-            ['A','B','D','E'], 
-            ['C','A','D','B','F'], 
-            ['D','A','H','B','C'], 
+            ['A','B','D','E'],
+            ['C','A','D','B','F'],
+            ['D','A','H','B','C'],
             ['E','F','B','C','E']
         ]
         self.assertEqual(getOccurence('A',transactions),3)
         self.assertEqual(getOccurence('H',transactions),1)
-        
+
+    def test_strToSet(self):
+        self. assertEqual(strToSet("ABC"), set(['A','B','C']))
+        self. assertEqual(strToSet("ABDE"), set(['A','B','E','D']))
+
+    def test_setToStr(self):
+        self.assertEqual(setToStr(set(['A', 'B', 'C'])), "ABC")
+        self.assertEqual(setToStr(set(['D','B','C'])), 'BCD')
+
+    def test_getSubset(self):
+        ens = set(['A', 'B', 'C'])
+        self.assertIn(set(['A']), getSubset(ens))
+        self.assertIn(set(['B','C']), getSubset(ens))
+
+    def test_setRules(self):
+        occurence = [
+            [
+                ('A',3),
+                ('B',4),
+                ('C',3)
+            ],
+            [
+                ('AB',3),
+                ('AC',3),
+                ('BC',3),
+            ],
+            [
+                ('ABC',3)
+            ]
+        ]
+        self.assertIn(
+            (set(['A']),set(['B']),1), setRules(occurence, 0.75)
+        )
+        self.assertIn(
+            (set(['B']),set(['A']),0.75), setRules(occurence, 0.75)
+        )
+        self.assertNotIn(
+            (set(['B']),set(['A']),0.75), setRules(occurence, 0.8)
+        )
+        self.assertIn(
+            (set(['A','B']),set(['C']),1), setRules(occurence, 0.75)
+        )
+    def test_getLabels(self):
+        occurence_dict = [
+            ('A',5),
+            ('B',6),
+            ('BC', 7),
+            ('ABC', 4)
+        ]
+        self.assertIn('A', getLabels(occurence_dict))
+        self.assertIn('BC', getLabels(occurence_dict))
+        self.assertNotIn('AB', getLabels(occurence_dict))
+        self.assertIn('ABC', getLabels(occurence_dict))
+    def test_apriori(self):
+        transactions = [
+            ['A','B','D','E'],
+            ['C','A','D','B','F'],
+            ['D','A','H','B','C'],
+            ['E','F','B','C','E']
+        ]
+        self.assertIn(
+            (set(['A']), set(['B']), 0.75),
+            apriori(transactions)
+        )
+
 if __name__ == "__main__":
     unittest.main()
